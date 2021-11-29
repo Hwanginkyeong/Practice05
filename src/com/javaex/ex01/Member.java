86b2d75ba09541ef55df1e0a457f7d7716e7ef53
hwanginkyeong <inke0831@naver.com>
2021-11-30 00:40:59 +0900

    1st

--- src/com/javaex/ex01/Member.java
+++ src/com/javaex/ex01/Member.java
@@ -2,6 +2,63 @@ package com.javaex.ex01;
 
 public class Member {
 	
-	private 
+	//필드
+	private String id;
+	private String name;
+	private int point;
+	
+	//생성자
+	public Member() {
+	}
+
+	public Member(String id, String name, int point) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.point = point;
+	}
+
+	//메소드 g/s
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
+	@Override
+	public String toString() {
+		return "Member [id=" + id + ", name=" + name + ", point=" + point + "]";
+	}
+	
+	
+	public void showInfo() {
+		System.out.println("회원정보: " +name+(id)+", ");
+		//System.out.println("회원정보: " +name+(id)+", ");
+		//System.out.println("회원정보: " +name+(id)+", ");
+		
+	}
+	
+	
+	
+	
+	
 
 }
